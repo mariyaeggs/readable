67c8200a71cb16c6d5ad6efdac0f12bd900d3087
Mariya Eggensperger <mariyawilson@att.net>
2018-04-17 18:41:16 -0700

    Remove unnecessary functions and lint errors (#11)

--- readable-client/src/utils/BooksAPI.js
+++ readable-client/src/utils/BooksAPI.js
@@ -1,32 +1,28 @@
-//const api = "mysql+pymysql://mariyaeggs:readable@dbreadable.czp7awvfoore.us-east-1.rds.amazonaws.com/dbreadable"
+/**
+ * @file BooksAPI - all endpoints in use by application.
+ */
+
+
+// API Constant
 const api = 'http://readable-server-dev.us-east-1.elasticbeanstalk.com/';
-// Generate a unique token for storing bookshelf data on the backend server
-let token = localStorage.token
-if (!token)
-  token = localStorage.token = Math.random().toString(36).substr(-8)
 
+// Headers to be included with each request
 const headers = {
-  'Accept': 'application/json',
-  'Authorization': token
-}
-
-export const get = (bookId) =>
-  fetch(`${api}/books/${bookId}`, { headers })
-    .then(res => res.json())
-    .then(data => data.book);
+  Accept: 'application/json',
+};
 
 export const getAll = () =>
   fetch(`${api}/books`, { headers })
-    .then(res => res.json())
+    .then(res => res.json());
 
 export const updateShelf = (bookId, shelf) =>
   fetch(`${api}/book/${bookId}`, {
     method: 'PUT',
     headers: {
       ...headers,
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ shelf })
+    body: JSON.stringify({ shelf }),
   }).then(res => res.json());
 
 export const removeBook = bookId =>
@@ -37,7 +33,7 @@ export const createBook = bookData =>
     method: 'POST',
     headers: {
       ...headers,
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(bookData),
   }).then(res => res.json());
